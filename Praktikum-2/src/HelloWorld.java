39b958017f9363811f8fcf996b1815bfe004fc15
Rahmad Madani <rahmatmadani85@gmail.com>
2026-09-19 23:54:41 +0700

    A.3. Komentar Kode

--- Praktikum-2/src/HelloWorld.java
+++ Praktikum-2/src/HelloWorld.java
@@ -1,8 +1,12 @@
 public class HelloWorld {
     public static void main(String[] args) {
                 System.out.println("Rahmad Madani");
+                // Program menampilkan biodata sederhana
                 System.out.print("Nama: ");
                 System.out.print("Rahmad");
+                /* Baris di bawah ini
+                mencetak NIM mahasiswa */
+                System.out.println("NIM: 2025573010062");
                 System.out.println();
                 System.out.println("Ini baris baru");
 
