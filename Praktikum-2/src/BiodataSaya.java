55ed8ef4f9bfef2528582f35677c1f68f592c0dc
Rahmad Madani <rahmatmadani85@gmail.com>
2026-09-20 00:11:49 +0700

    latihan 1

---
+++ Praktikum-2/src/BiodataSaya.java
@@ -0,0 +1,19 @@
+import java.util.Scanner; // mengimport library untuk menerima inputan user
+
+public class BiodataSaya {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("nama : ");
+        String nama = sc.nextLine();
+
+        System.out.print("NIM : ");
+        String nim = sc.nextLine(); // untuk menerima inputan dari user
+
+        System.out.print("Program Studi : ");
+        String prodi = sc.nextLine();
+
+        System.out.println("Nama : " + nama + " \nNIM : " + nim + " \nProgram Studi : " + prodi);
+
+    }
+}
